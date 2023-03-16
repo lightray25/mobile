25877db011154def0c33b4ef8b0e323b1cf3dcb0
Siegfried Obenauer <siegfriedobenauer@MacBook-Pro-2.local>
2023-03-16 11:05:56 +0100

    more pages and js foundation

---
+++ name.js
@@ -0,0 +1,19 @@
+addEventListener('DOMContentLoaded', (event) => {
+
+  const firstname = document.querySelector('#firstname');
+  const lastname = document.querySelector('#lastname');
+  const button = document.querySelector('#button');
+  let output = document.getElementById('output');
+  const list = [];
+
+  console.log(firstname.value, lastname.value);
+
+  button.addEventListener("click", e => {
+    console.log(firstname.value, lastname.value);
+    list.push(firstname.value, lastname.value)
+    window.location = "plz.html";
+  })
+
+});
+
+CondomcontentLoaded = (event) => {  };
